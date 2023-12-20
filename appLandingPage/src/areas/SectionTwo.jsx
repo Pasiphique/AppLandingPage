06f8567f9e60747c1667b9c6bb15a4b3b6030a00
Pasiphique <pasiphiqueoswald@gmail.com>
2023-12-20 13:20:49 -0500

    finished adding simple css for section two

--- appLandingPage/src/areas/SectionTwo.jsx
+++ appLandingPage/src/areas/SectionTwo.jsx
@@ -4,13 +4,15 @@ import learnMore from '../img/options/learn-more-ow.png'
 export default function sectionTwo(){
     return(
         <div className="section section-two">
-            <div>
-                <h1>External Checking Account</h1>
-                <p>Keep your current bank and have your Empower Cash Advance or Empower Thrive funds deposited to your primary bank account.</p>
-                <button><img src={learnMore} alt="" /></button>
+            <div className='section-two-infoContainer'>
+                <div className='section-two-info'>
+                    <h1 className='section-two-header'>External Checking Account</h1>
+                    <p>Keep your current bank and have your Empower Cash Advance or Empower Thrive funds deposited to your primary bank account.</p>
+                    <button><img src={learnMore} alt="" /></button>
+                </div>
             </div>
             <div>
-            <img src={phone} alt="" className='phone-container' />
+                <img src={phone} alt="" className='phone-container' />
             </div>
             
         </div>

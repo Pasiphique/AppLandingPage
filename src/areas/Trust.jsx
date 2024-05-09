930ef80383248517088ae8f1c15663f7fcc4d766
Pasiphique <pasiphiqueoswald@gmail.com>
2024-05-08 20:23:45 -0400

    added the trust section and added styling

--- src/areas/Trust.jsx
+++ src/areas/Trust.jsx
@@ -1,14 +1,14 @@
 import Users from "../components/Users"
 export default function Trust(){
     return(
-        <div className="trust section-two">
-            <div>Trusted by millions of customers.</div>
+        <div className="trust">
+            <p>Trusted by millions of customers.</p>
             <div>
-                <Users name="Bob" /> 
-                <Users name="Dave" /> 
-                <Users name="Eddy" /> 
-                <Users name="Frank" />  
+                <Users  /> 
+                
             </div>
-        </div> 
+              
+        </div>
+
     )
 }
--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,9 +1,21 @@
 
-
-export default function Users({info, avatar,name, title}){
+import UserInfo from './UserInfo.json'
+export default function Users(){
     return(
-        <div>
-            {name}
+        <div className='profile-container'>
+            {UserInfo.map((user,index)=>(
+                <div className='user-profile' key={index}>
+                    <p>{user.description}</p>
+                    <div>
+                        <img src={user.src} alt="avatar" />
+                        <div className='user-name'>
+                            <p>{user.name}</p>
+                            <p>{user.job}</p>
+                        </div>
+                    </div>
+            </div>
+            ))}
         </div>
+        
     )
 }
